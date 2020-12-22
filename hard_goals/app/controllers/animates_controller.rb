cab7f9b9b40faa99db01940169dde7c240130ebd
Bruce <47602871+bruffin1221@users.noreply.github.com>
2020-12-21 21:46:58 -0800

    Worked on views for heartfelt ans animate

--- hard_goals/app/controllers/animates_controller.rb
+++ hard_goals/app/controllers/animates_controller.rb
@@ -2,36 +2,50 @@ class AnimatesController < ApplicationController
 
   # GET: /animates
   get "/animates" do
+    @animates=Animate.all
     erb :"/animates/index.html"
   end
 
   # GET: /animates/new
   get "/animates/new" do
+    @heartfelts=Heartfelt.all
     erb :"/animates/new.html"
   end
 
   # POST: /animates
   post "/animates" do
-    redirect "/animates"
+    goal=params["heart"]["heart_ids"]
+    @goal= Heartfelt.find_by(name: goal.first)
+    @animate=Animate.find_or_create_by(name: params[:name], description: params[:description], size: params[:size],
+    color: params[:color], emotion: params[:emotion], setting: params[:setting], movement: params[:movement],
+    distinct_parts: params[:distinct_parts], heartfelt_id: @goal.id)
+    redirect "/animates/#{@animate.id}"
   end
 
   # GET: /animates/5
   get "/animates/:id" do
+    @animate=Animate.find_by_id(params[:id])
     erb :"/animates/show.html"
   end
 
   # GET: /animates/5/edit
   get "/animates/:id/edit" do
+    @animate= Animate.find_by_id(params[:id])
     erb :"/animates/edit.html"
   end
 
   # PATCH: /animates/5
   patch "/animates/:id" do
-    redirect "/animates/:id"
+    @animate=Animate.find_by_id(params[:id])
+    @animate.update(name: params[:name], description: params[:description], size: params[:size],
+    color: params[:color], emotion: params[:emotion], setting: params[:setting], movement: params[:movement],
+    distinct_parts: params[:distinct_parts])
+    redirect "/animates/#{@animate.id}"
   end
 
   # DELETE: /animates/5/delete
-  delete "/animates/:id/delete" do
+  delete "/animates/:id" do
+    Animate.find_by_id(params[:id]).destroy
     redirect "/animates"
   end
 end
--- hard_goals/app/controllers/heartfelts_controller.rb
+++ hard_goals/app/controllers/heartfelts_controller.rb
@@ -13,31 +13,37 @@ class HeartfeltsController < ApplicationController
 
   # POST: /heartfelts
   post "/heartfelts" do
-    @heartfelts=Heartfelt.find_or_create_by(name: params[:heartfelt][:name], description: params[:heartfelt][:description], 
-    enjoyments: params[:heartfelt][:enjoyments], benefits: params[:heartfelt][:benefits], rewards: params[:heartfelt][:rewards]) 
-    redirect "/heartfelts"
-  end
-
-  # GET: /heartfelts/5
-  get "/heartfelts/:id" do
-    @heartfelt=Heartfelt.find_by_id(params[:id])
-    erb :"/heartfelts/show.html"
+    @heartfelts=Heartfelt.find_or_create_by(name: params[:name], description: params[:description], enjoyments: params[:enjoyments], benefits: params[:benefits], rewards: params[:rewards]) 
+    redirect "/heartfelts/#{@heartfelts.id}"
   end
 
   # GET: /heartfelts/5/edit
   get "/heartfelts/:id/edit" do
+    @heartfelts= Heartfelt.find_by_id(params[:id]) 
     erb :"/heartfelts/edit.html"
   end
 
   # PATCH: /heartfelts/5
   patch "/heartfelts/:id" do
-    redirect "/heartfelts/:id"
+    @heartfelts=Heartfelt.find_by_id(params[:id])
+    @heartfelts.update(name: params[:name], description: params[:description], enjoyments: params[:enjoyments], benefits: params[:benefits], rewards: params[:rewards])
+    redirect "/heartfelts/#{@heartfelts.id}"
   end
 
   # DELETE: /heartfelts/5/delete
-  delete "/heartfelts/:id/delete" do
+  delete "/heartfelts/:id" do
+    Heartfelt.find_by_id(params[:id]).destroy
     redirect "/heartfelts"
   end
+
+   # GET: /heartfelts/5
+   get "/heartfelts/:id" do
+    @heartfelts=Heartfelt.find_by_id(params[:id])
+    erb :"/heartfelts/show.html"
+  end
+
+    
 end
 
-params={"heartfelt"=>{"name"=>"wwww", "description"=>"eenenene", "enjoyments"=>"emememe", "benefits"=>"ememe", "rewards"=>"emememe"}}
+   
+
